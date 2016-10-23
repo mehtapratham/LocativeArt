406af0638d6889e906967963cbc03bdeb73dfacc
Pratham Mehta <pbm1992@gmail.com>
2016-10-22 22:36:57 -0400

    Add location identification.

--- LocativeArt/app/src/main/java/com/vuforia/samples/VuforiaSamples/ui/ActivityList/ActivityLauncher.java
+++ LocativeArt/app/src/main/java/com/vuforia/samples/VuforiaSamples/ui/ActivityList/ActivityLauncher.java
@@ -10,16 +10,43 @@ countries.
 
 package com.vuforia.samples.VuforiaSamples.ui.ActivityList;
 
+import android.Manifest;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.vuforia.samples.VuforiaSamples.R;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 
 // This activity starts activities which demonstrate the Vuforia features
@@ -27,8 +54,11 @@ public class ActivityLauncher extends ListActivity
 {
     
     private String mActivities[] = { "Image Targets" };
-    
-    
+
+    private LocationUpdater mLocationUpdater;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mRequestingLocationUpdates = true;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -42,6 +72,19 @@ public class ActivityLauncher extends ListActivity
         
         setContentView(R.layout.activities_list);
         setListAdapter(adapter);
+
+
+        mLocationUpdater = new LocationUpdater();
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+            // See https://g.co/AppIndexing/AndroidStudio for more information.
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(mLocationUpdater)
+                    .addOnConnectionFailedListener(mLocationUpdater)
+                    .addApi(LocationServices.API)
+                    .addApi(AppIndex.API).build();
+        }
     }
     
     
@@ -59,55 +102,149 @@ public class ActivityLauncher extends ListActivity
                     "app.ImageTargets.ImageTargets");
                 intent.putExtra("ABOUT_TEXT", "ImageTargets/IT_about.html");
                 break;
-//            case 1:
-//                intent.putExtra("ACTIVITY_TO_LAUNCH",
-//                        "app.VuMark.VuMark");
-//                intent.putExtra("ABOUT_TEXT", "VuMark/VM_about.html");
-//                break;
-//            case 2:
-//                intent.putExtra("ACTIVITY_TO_LAUNCH",
-//                    "app.CylinderTargets.CylinderTargets");
-//                intent.putExtra("ABOUT_TEXT", "CylinderTargets/CY_about.html");
-//                break;
-//            case 3:
-//                intent.putExtra("ACTIVITY_TO_LAUNCH",
-//                    "app.MultiTargets.MultiTargets");
-//                intent.putExtra("ABOUT_TEXT", "MultiTargets/MT_about.html");
-//                break;
-//            case 4:
-//                intent.putExtra("ACTIVITY_TO_LAUNCH",
-//                    "app.UserDefinedTargets.UserDefinedTargets");
-//                intent.putExtra("ABOUT_TEXT",
-//                    "UserDefinedTargets/UD_about.html");
-//                break;
-//            case 5:
-//                intent.putExtra("ACTIVITY_TO_LAUNCH",
-//                    "app.ObjectRecognition.ObjectTargets");
-//                intent.putExtra("ABOUT_TEXT", "ObjectRecognition/OR_about.html");
-//                break;
-//            case 6:
-//                intent.putExtra("ACTIVITY_TO_LAUNCH",
-//                    "app.CloudRecognition.CloudReco");
-//                intent.putExtra("ABOUT_TEXT", "CloudReco/CR_about.html");
-//                break;
-//            case 7:
-//                intent.putExtra("ACTIVITY_TO_LAUNCH",
-//                    "app.TextRecognition.TextReco");
-//                intent.putExtra("ABOUT_TEXT", "TextReco/TR_about.html");
-//                break;
-//            case 8:
-//                intent.putExtra("ACTIVITY_TO_LAUNCH",
-//                    "app.FrameMarkers.FrameMarkers");
-//                intent.putExtra("ABOUT_TEXT", "FrameMarkers/FM_about.html");
-//                break;
-//            case 9:
-//                intent.putExtra("ACTIVITY_TO_LAUNCH",
-//                    "app.VirtualButtons.VirtualButtons");
-//                intent.putExtra("ABOUT_TEXT", "VirtualButtons/VB_about.html");
-//                break;
         }
         
         startActivity(intent);
         
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationUpdater.stopLocationUpdates();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            mLocationUpdater.startLocationUpdates();
+        }
+    }
+
+    private class LocationUpdater implements
+            ConnectionCallbacks,OnConnectionFailedListener, LocationListener {
+
+        private static final int REQUEST_CHECK_SETTINGS = 1000;
+        private Location mLastLocation;
+        private LocationRequest mLocationRequest;
+        private Location mCurrentLocation;
+
+        @Override
+        public void onConnected(@Nullable Bundle bundle) {
+            if (ActivityCompat.checkSelfPermission(ActivityLauncher.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(ActivityLauncher.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+            if (mLastLocation != null) {
+//                mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+//                mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+            }
+
+            createLocationRequest();
+
+
+            if(mRequestingLocationUpdates) {
+                startLocationUpdates();
+            }
+        }
+
+        private void createLocationRequest() {
+            mLocationRequest = new LocationRequest();
+            mLocationRequest.setInterval(2000);
+            mLocationRequest.setFastestInterval(1000);
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                    .addLocationRequest(mLocationRequest);
+
+            PendingResult<LocationSettingsResult> result =
+                    LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
+                            builder.build());
+
+            result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+                @Override
+                public void onResult(LocationSettingsResult result) {
+                    final Status status = result.getStatus();
+//                final LocationSettingsStates= result.getLocationSettingsStates();
+                    switch (status.getStatusCode()) {
+                        case LocationSettingsStatusCodes.SUCCESS:
+                            // All location settings are satisfied. The client can
+                            // initialize location requests here.
+
+                            break;
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            // Location settings are not satisfied, but this can be fixed
+                            // by showing the user a dialog.
+                            try {
+                                // Show the dialog by calling startResolutionForResult(),
+                                // and check the result in onActivityResult().
+                                status.startResolutionForResult(
+                                        ActivityLauncher.this,
+                                        REQUEST_CHECK_SETTINGS);
+                            } catch (IntentSender.SendIntentException e) {
+                                // Ignore the error.
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            // Location settings are not satisfied. However, we have no way
+                            // to fix the settings so we won't show the dialog.
+
+                            break;
+                    }
+                }
+            });
+        }
+
+        public void startLocationUpdates() {
+            if (ActivityCompat.checkSelfPermission(ActivityLauncher.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(ActivityLauncher.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+
+        public void stopLocationUpdates() {
+            LocationServices.FusedLocationApi.removeLocationUpdates(
+                    mGoogleApiClient, this);
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {}
+
+        @Override
+        public void onLocationChanged(Location location) {
+            mCurrentLocation = location;
+//            mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        }
+
+        @Override
+        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {}
     }
 }
